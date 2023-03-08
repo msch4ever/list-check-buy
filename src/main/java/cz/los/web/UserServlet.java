450c59233b9dcd302d5a3321e7d717e6d938e710
Kostia <msch4ever@gmail.com>
2023-03-08 04:38:44 +0100

    user servlet split

--- src/main/java/cz/los/web/UserServlet.java
+++ src/main/java/cz/los/web/UserServlet.java
@@ -1,11 +1,13 @@
 package cz.los.web;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.los.model.User;
 import cz.los.service.UserService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,70 +16,77 @@ import java.util.Optional;
 
 import static cz.los.service.UserService.USER_SERVICE;
 
-@WebServlet(name = "UserServlet", value = "/users/*")
 public class UserServlet extends HttpServlet {
 
-    private UserService userService;
+    @NoArgsConstructor
+    @WebServlet(name = "GetUserServlet", value = "/users/*")
+    public static class GetUserServlet extends HttpServlet {
+        private UserService userService;
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        ServletContext servletContext = config.getServletContext();
-        userService = (UserService) servletContext.getAttribute(USER_SERVICE);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String pathInfo = request.getPathInfo();
-        boolean listPattern = pathInfo == null || pathInfo.equals("/");
-        if (listPattern) {
-            listUsers(response);
-            return;
+        @Override
+        public void init(ServletConfig config) throws ServletException {
+            super.init(config);
+            ServletContext servletContext = config.getServletContext();
+            userService = (UserService) servletContext.getAttribute(USER_SERVICE);
         }
-        boolean getByIdPattern = pathInfo.matches("/\\d+");
-        if (getByIdPattern) {
-            getUser(response, pathInfo);
-            return;
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String pathInfo = request.getPathInfo();
+            boolean listPattern = pathInfo == null || pathInfo.equals("/");
+            if (listPattern) {
+                listUsers(response);
+                return;
+            }
+            boolean getByIdPattern = pathInfo.matches("/\\d+");
+            if (getByIdPattern) {
+                getUser(response, pathInfo);
+                return;
+            }
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Wrong get operation on user");
         }
-        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Wrong get operation on user");
-    }
 
-    private void listUsers(HttpServletResponse response) throws IOException {
-        List<String> users = userService.getAllUsers();
-        response.setContentType("application/json");
-        PrintWriter writer = response.getWriter();
-        writer.write("{\"users\":[");
-        String combinedUsers = String.join(",", users);
-        writer.write(combinedUsers);
-        writer.write("]}");
-    }
-
-    private void getUser(HttpServletResponse response, String pathInfo) throws IOException {
-        long id = Long.parseLong(pathInfo.substring(1));
-        Optional<String> user = userService.getUserById(id);
-        if (user.isPresent()) {
+        private void listUsers(HttpServletResponse response) throws IOException {
+            List<String> users = userService.getAllUsers();
             response.setContentType("application/json");
-            response.getWriter().write(user.get());
-        } else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            PrintWriter writer = response.getWriter();
+            writer.write("{\"users\":[");
+            String combinedUsers = String.join(",", users);
+            writer.write(combinedUsers);
+            writer.write("]}");
+        }
+
+        private void getUser(HttpServletResponse response, String pathInfo) throws IOException {
+            long id = Long.parseLong(pathInfo.substring(1));
+            Optional<String> user = userService.getUserById(id);
+            if (user.isPresent()) {
+                response.setContentType("application/json");
+                response.getWriter().write(user.get());
+            } else {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
         }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo != null && pathInfo.equals("/create")) {
+    @NoArgsConstructor
+    @WebServlet(name = "PostUserServlet", value = "/createUser")
+    public static class PostUserServlet extends HttpServlet {
+
+        private UserService userService;
+
+        @Override
+        public void init(ServletConfig config) throws ServletException {
+            super.init(config);
+            ServletContext servletContext = config.getServletContext();
+            userService = (UserService) servletContext.getAttribute(USER_SERVICE);
+        }
+
+        @Override
+        public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             ObjectMapper objectMapper = new ObjectMapper();
-            User user = objectMapper.readValue(request.getReader(), User.class);
-            userService.createUser(user.getName());
+            JsonNode jsonNode = objectMapper.readTree(request.getReader());
+            userService.createUser(jsonNode.get("name").textValue());
             response.setStatus(HttpServletResponse.SC_CREATED);
-        } else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
-    }
-
-    @WebServlet
-    private class GetUserServlet extends HttpServlet {
-
     }
 }
